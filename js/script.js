f44b934f5bef2bc29125c49d61359e5e9dbad711
sp <jazzp10.5@gmail.com>
2021-03-17 20:03:00 -0500

    teclado taller

---
+++ js/script.js
@@ -0,0 +1,448 @@
+let btnq=document.getElementById("btnq");
+let btnw=document.getElementById("btnw");
+let btne=document.getElementById("btne");
+let btnr=document.getElementById("btnr");
+let btnt=document.getElementById("btnt");
+let btny=document.getElementById("btny");
+let btnu=document.getElementById("btnu");
+let btni=document.getElementById("btni");
+let btno=document.getElementById("btno");
+let btnp=document.getElementById("btnp");
+let btna=document.getElementById("btna");
+let btns=document.getElementById("btns");
+let btnd=document.getElementById("btnd");
+let btnf=document.getElementById("btnf");
+let btng=document.getElementById("btng");
+let btnh=document.getElementById("btnh");
+let btnj=document.getElementById("btnj");
+let btnk=document.getElementById("btnk");
+let btnl=document.getElementById("btnl");
+let btnñ=document.getElementById("btnñ");
+let btnz=document.getElementById("btnz");
+let btnx=document.getElementById("btnx");
+let btnc=document.getElementById("btnc");
+let btnv=document.getElementById("btnv");
+let btnb=document.getElementById("btnb");
+let btnn=document.getElementById("btnn");
+let btnm=document.getElementById("btnm");
+let btncoma=document.getElementById("btncoma");
+let btnpunto=document.getElementById("btnpunto");
+let btnraya=document.getElementById("btnraya");
+let btnespacio=document.getElementById("btnespacio");
+
+let btnenter=document.getElementById("btnenter");
+let pantalla=document.getElementById("pantalla");
+
+let btnNum1 = document.getElementById("btnNum1");
+let btnNum2 = document.getElementById("btnNum2");
+let btnNum3 = document.getElementById("btnNum3");
+let btnNum4 = document.getElementById("btnNum4");
+let btnNum5 = document.getElementById("btnNum5");
+let btnNum6 = document.getElementById("btnNum6");
+let btnNum7 = document.getElementById("btnNum7");
+let btnNum8 = document.getElementById("btnNum8");
+let btnNum9 = document.getElementById("btnNum9");
+let btnNum0 = document.getElementById("btnNum0");
+
+let btnmayus=document.getElementById("btnmayus");
+
+let btborrar=document.getElementById("btnborrar");
+
+let mayus=false;
+
+const btnqF =() => {  
+    if (mayus===true) {
+        pantalla.innerHTML=pantalla.innerHTML+"Q";
+    } else {
+        pantalla.innerHTML=pantalla.innerHTML+"q";
+    }
+   
+}
+const btnwF =() => {  
+    if (mayus===true) {
+        pantalla.innerHTML=pantalla.innerHTML+"W";
+    } else {
+        pantalla.innerHTML=pantalla.innerHTML+"w";
+    }
+}
+const btneF =() => {  
+    if (mayus===true) {
+        pantalla.innerHTML=pantalla.innerHTML+"E";
+    } else {
+        pantalla.innerHTML=pantalla.innerHTML+"e";
+    }
+}
+const btnrF =() => {  
+    if (mayus===true) {
+        pantalla.innerHTML=pantalla.innerHTML+"R";
+    } else {
+        pantalla.innerHTML=pantalla.innerHTML+"r";
+    }
+}
+const btntF =() => {  
+    if (mayus===true) {
+        pantalla.innerHTML=pantalla.innerHTML+"T";
+    } else {
+        pantalla.innerHTML=pantalla.innerHTML+"t";
+    }
+}
+const btnyF =() => {  
+    if (mayus===true) {
+        pantalla.innerHTML=pantalla.innerHTML+"Y";
+    } else {
+        pantalla.innerHTML=pantalla.innerHTML+"y";
+    }
+}
+const btnuF =() => {  
+    if (mayus===true) {
+        pantalla.innerHTML=pantalla.innerHTML+"U";
+    } else {
+        pantalla.innerHTML=pantalla.innerHTML+"u";
+    }
+}
+const btniF =() => {  
+    if (mayus===true) {
+        pantalla.innerHTML=pantalla.innerHTML+"I";
+    } else {
+        pantalla.innerHTML=pantalla.innerHTML+"i";
+    }
+}
+const btnoF =() => {  
+    if (mayus===true) {
+        pantalla.innerHTML=pantalla.innerHTML+"O";
+    } else {
+        pantalla.innerHTML=pantalla.innerHTML+"o";
+    }
+}
+const btnpF =() => {  
+    if (mayus===true) {
+        pantalla.innerHTML=pantalla.innerHTML+"P";
+    } else {
+        pantalla.innerHTML=pantalla.innerHTML+"p";
+    }
+}
+const btnaF =() => {  
+    if (mayus===true) {
+        pantalla.innerHTML=pantalla.innerHTML+"A";
+    } else {
+        pantalla.innerHTML=pantalla.innerHTML+"a";
+    }
+}
+const btnsF =() => {  
+    if (mayus===true) {
+        pantalla.innerHTML=pantalla.innerHTML+"S";
+    } else {
+        pantalla.innerHTML=pantalla.innerHTML+"s";
+    }
+}
+const btndF =() => {  
+    if (mayus===true) {
+        pantalla.innerHTML=pantalla.innerHTML+"D";
+    } else {
+        pantalla.innerHTML=pantalla.innerHTML+"d";
+    }
+}
+const btnfF =() => {  
+    if (mayus===true) {
+        pantalla.innerHTML=pantalla.innerHTML+"F";
+    } else {
+        pantalla.innerHTML=pantalla.innerHTML+"f";
+    }
+}
+const btngF =() => {  
+    if (mayus===true) {
+        pantalla.innerHTML=pantalla.innerHTML+"G";
+    } else {
+        pantalla.innerHTML=pantalla.innerHTML+"g";
+    }
+}
+const btnhF =() => {  
+    if (mayus===true) {
+        pantalla.innerHTML=pantalla.innerHTML+"H";
+    } else {
+        pantalla.innerHTML=pantalla.innerHTML+"h";
+    }
+}
+const btnjF =() => {  
+    if (mayus===true) {
+        pantalla.innerHTML=pantalla.innerHTML+"J";
+    } else {
+        pantalla.innerHTML=pantalla.innerHTML+"j";
+    }
+}
+const btnkF =() => {  
+    if (mayus===true) {
+        pantalla.innerHTML=pantalla.innerHTML+"K";
+    } else {
+        pantalla.innerHTML=pantalla.innerHTML+"k";
+    }
+}
+const btnlF =() => {  
+    if (mayus===true) {
+        pantalla.innerHTML=pantalla.innerHTML+"L";
+    } else {
+        pantalla.innerHTML=pantalla.innerHTML+"l";
+    }
+}
+const btnñF =() => {  
+    if (mayus===true) {
+        pantalla.innerHTML=pantalla.innerHTML+"Ñ";
+    } else {
+        pantalla.innerHTML=pantalla.innerHTML+"ñ";
+    }
+}
+const btnzF =() => {  
+    if (mayus===true) {
+        pantalla.innerHTML=pantalla.innerHTML+"Z";
+    } else {
+        pantalla.innerHTML=pantalla.innerHTML+"z";
+    }
+}
+const btnxF =() => {  
+    if (mayus===true) {
+        pantalla.innerHTML=pantalla.innerHTML+"X";
+    } else {
+        pantalla.innerHTML=pantalla.innerHTML+"x";
+    }
+}
+const btncF =() => {  
+    if (mayus===true) {
+        pantalla.innerHTML=pantalla.innerHTML+"C";
+    } else {
+        pantalla.innerHTML=pantalla.innerHTML+"c";
+    }
+}
+const btnvF =() => {  
+    if (mayus===true) {
+        pantalla.innerHTML=pantalla.innerHTML+"V";
+    } else {
+        pantalla.innerHTML=pantalla.innerHTML+"v";
+    }
+}
+const btnbF =() => {  
+    if (mayus===true) {
+        pantalla.innerHTML=pantalla.innerHTML+"B";
+    } else {
+        pantalla.innerHTML=pantalla.innerHTML+"b";
+    }
+}
+const btnnF =() => {  
+    if (mayus===true) {
+        pantalla.innerHTML=pantalla.innerHTML+"N";
+    } else {
+        pantalla.innerHTML=pantalla.innerHTML+"n";
+    }
+}
+const btnmF =() => {  
+    if (mayus===true) {
+        pantalla.innerHTML=pantalla.innerHTML+"M";
+    } else {
+        pantalla.innerHTML=pantalla.innerHTML+"m";
+    }
+}
+const btncomaF =() => {  
+    pantalla.innerHTML=pantalla.innerHTML+",";
+}
+const btnpuntoF =() => {  
+    pantalla.innerHTML=pantalla.innerHTML+".";
+}
+const btnrayaF =() => {  
+    pantalla.innerHTML=pantalla.innerHTML+"-";
+}
+const btnespacioF =() => {  
+    pantalla.innerHTML=pantalla.innerHTML + " ";
+}
+const btnenterF =() => {  
+    pantalla.innerHTML=pantalla.innerHTML + "<br>";
+}
+const btnmayusF =() => {  
+
+    if (mayus===false) {
+        mayus=true;
+    } else {
+        mayus=false;
+    }
+    
+}
+const btnborrarF =() => {  
+    pantalla.innerHTML="";
+}
+//numeros
+
+const btnNum1F = () => {
+    pantalla.innerHTML = pantalla.innerHTML + "1";
+  };
+  const btnNum2F = () => {
+    pantalla.innerHTML = pantalla.innerHTML + "2";
+  };
+  const btnNum3F = () => {
+    pantalla.innerHTML = pantalla.innerHTML + "3";
+  };
+  const btnNum4F = () => {
+    pantalla.innerHTML = pantalla.innerHTML + "4";
+  };
+  const btnNum5F = () => {
+    pantalla.innerHTML = pantalla.innerHTML + "5";
+  };
+  const btnNum6F = () => {
+    pantalla.innerHTML = pantalla.innerHTML + "6";
+  };
+  const btnNum7F = () => {
+    pantalla.innerHTML = pantalla.innerHTML + "7";
+   
+  };
+  const btnNum8F = () => {
+    pantalla.innerHTML = pantalla.innerHTML + "8";
+  };
+  const btnNum9F = () => {
+    pantalla.innerHTML = pantalla.innerHTML + "9";
+  };
+  const btnNum0F = () => {
+    pantalla.innerHTML = pantalla.innerHTML + "0";
+  };
+
+
+
+//onclick
+btnq.onclick = function () {
+    btnqF();
+  };
+  btnw.onclick = function () {
+    btnwF();
+  };
+  btne.onclick = function () {
+    btneF();
+  };
+  btnr.onclick = function () {
+    btnrF();
+  };
+  btnt.onclick = function () {
+    btntF();
+  };
+  btny.onclick = function () {
+    btnyF();
+  };
+  btnu.onclick = function () {
+    btnuF();
+  };
+  btni.onclick = function () {
+    btniF();
+  };
+  btno.onclick = function () {
+    btnoF();
+  };
+  btnp.onclick = function () {
+    btnpF();
+  };
+  btna.onclick = function () {
+    btnaF();
+  };
+  btns.onclick = function () {
+    btnsF();
+  };
+  btnd.onclick = function () {
+    btndF();
+  };
+  btnf.onclick = function () {
+    btnfF();
+  };
+  btng.onclick = function () {
+    btngF();
+  };
+  btnh.onclick = function () {
+    btnhF();
+  };
+  btnj.onclick = function () {
+    btnjF();
+  };
+  btnk.onclick = function () {
+    btnkF();
+  };
+  btnl.onclick = function () {
+    btnlF();
+  };
+  btnñ.onclick = function () {
+    btnñF();
+  };
+  btnz.onclick = function () {
+    btnzF();
+  };
+  btnx.onclick = function () {
+    btnxF();
+  };
+  btnc.onclick = function () {
+    btncF();
+  };
+  btnv.onclick = function () {
+    btnvF();
+  };
+  btnb.onclick = function () {
+    btnbF();
+  };
+  btnn.onclick = function () {
+    btnnF();
+  };
+  btnm.onclick = function () {
+    btnmF();
+  };
+  btncoma.onclick = function () {
+    btncomaF();
+  };
+  btnpunto.onclick = function () {
+    btnpuntoF();
+  };
+  btnraya.onclick = function () {
+    btnrayaF();
+  };
+
+  //espacio
+
+  btnespacio.onclick = function () {
+    btnespacioF();
+  };
+
+  //enter
+
+  btnenter.onclick = function () {
+    btnenterF();
+  };
+
+  btnmayus.onclick = function () {
+    btnmayusF();
+  };
+  btborrar.onclick=function(){
+      btnborrarF();
+  }
+
+  //numeros 
+
+  btnNum1.onclick = function () {
+    btnNum1F();
+  };
+  btnNum2.onclick = function () {
+    btnNum2F();
+  };
+  btnNum3.onclick = function () {
+    btnNum3F();
+  };
+  btnNum4.onclick = function () {
+    btnNum4F();
+  };
+  btnNum5.onclick = function () {
+    btnNum5F();
+  };
+  btnNum6.onclick = function () {
+    btnNum6F();
+  };
+  btnNum7.onclick = function () {
+    btnNum7F();
+  };
+  btnNum8.onclick = function () {
+    btnNum8F();
+  };
+  btnNum9.onclick = function () {
+    btnNum9F();
+  };
+  btnNum0.onclick = function () {
+    btnNum0F();
+  };
